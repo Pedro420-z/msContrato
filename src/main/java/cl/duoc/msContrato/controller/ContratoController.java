b74dbdcec1d2d46156a51a9a47271491b77109b1
bnmnnm <be.curinanco@duocuc.cl>
2026-05-17 19:51:18 -0400

    Refactor ContratoController to improve error handling

--- src/main/java/cl/duoc/msContrato/controller/ContratoController.java
+++ src/main/java/cl/duoc/msContrato/controller/ContratoController.java
@@ -3,7 +3,6 @@ package cl.duoc.msContrato.controller
 import cl.duoc.msContrato.model.Contrato;
 import cl.duoc.msContrato.service.ContratoService;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,14 +18,22 @@ public class ContratoController {
 
     @GetMapping
     public ResponseEntity<List<Contrato>> listar() {
-        return ResponseEntity.ok(contratoService.listarContratos());
+        try{
+            Contrato contrato = service.ListarContratos();
+            return ResponseEntity.ok(contrato);
+        }catch(Exception e){
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Contrato> obtenerPorId(@PathVariable Long id) {
+    public ResponseEntity<Contrato> obtenerPorId(@PathVariable Integer id) {
         try{
-            Contrato contrato = service.
+            Contrato contrato = service.buscarContrato(id);
             return ResponseEntity.ok();
+        } catch(Exception e){
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping
@@ -38,4 +45,6 @@ public class ContratoController {
             return RespondeEntity.notFound().build();
         }
     }
+
+    @PutMapping
 }
